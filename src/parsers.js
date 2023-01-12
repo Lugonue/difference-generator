cbb9b08e28a70cb711b6b8ad707bff5832c98d85
lugonue <lugonue@yandex.ru>
2023-01-12 17:49:10 +0300

    fix lint

--- src/parsers.js
+++ src/parsers.js
@@ -7,7 +7,7 @@ import yaml from 'js-yaml';
 const parser = (fileName) => { // принимает имя файла => возвращает объект содержимого в файле
     const extension = path.extname(fileName);
     const getPath = path.resolve(fileName);
-    return extension === '.json' ? JSON.parse(fs.readFileSync(getPath, 'utf8')) : (extension === '.yaml' || '.yml' ? yaml.load(fs.readFileSync(getPath, 'utf8')) : false);
+    return extension === '.json' ? JSON.parse(fs.readFileSync(getPath, 'utf8')) : (extension === '.yaml' || extension === '.yml' ? yaml.load(fs.readFileSync(getPath, 'utf8')) : false);
 }
 
 
