3c3de03e81dda0e1ac7ebd970bc7978647df4f84
lugonue <lugonue@yandex.ru>
2023-01-27 14:19:38 +0300

    fix main file

--- src/genDiffMain.js
+++ src/genDiffMain.js
@@ -2,7 +2,7 @@ import { compareFiles } from "./compareFiles.js"
 import { format } from "./formatters/index.js"
 import { parser } from "./parsers.js"
 
-const genDIffMain = (filepath1, filepath2, formatName) => {
+const genDIffMain = (filepath1, filepath2, formatName = 'stylish') => {
     const data1 = parser(filepath1);
     const data2 = parser(filepath2);
     const differenceData = compareFiles(data1, data2);
